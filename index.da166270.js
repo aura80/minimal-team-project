b4109762f5d2addfdf652a8f8b3454392ab6bac6
aura80 <96388713+aura80@users.noreply.github.com>
2024-09-11 15:48:07 +0000

    Deploying to gh-pages from @ aura80/minimal-team-project@a136c9c1e0847f51bee4d1dcf2fd116fa05949da 🚀

---
+++ index.da166270.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){let e=document.querySelector(".menu-btn"),n=document.querySelector(".menu-close-btn"),t=document.querySelector(".mobile-menu");e.addEventListener("click",function(){t.classList.add("is-open")}),n.addEventListener("click",function(){t.classList.remove("is-open")})});
+//# sourceMappingURL=index.da166270.js.map
